31ae68d05db1aeaabd08d8ec5144ff8597602735
lzjyzq2 <1462678018@qq.com>
2019-07-25 16:46:56 +0800

    新增资源和InfoActivity

---
+++ FileServerAndroid/wear/src/main/java/cn/settile/lzjyzq2/fileserver/InfoActivity.java
@@ -0,0 +1,15 @@
+package cn.settile.lzjyzq2.fileserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+
+public class InfoActivity extends WearableActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
